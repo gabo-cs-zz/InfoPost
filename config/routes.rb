d00c0438046cd135a1534fc9685a53685242aea0
Gabriel Coronado <gabrielomar2809@gmail.com>
2018-07-11 12:56:04 -0400

    Routes, Header, Flash

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'posts#index'
-  get 'posts', to: 'posts#index'
-  post 'posts', to: 'posts#create'
-  get 'posts/new', to: 'posts#new'
-  get 'posts/:id', to: 'posts#show', as: :post
-  get 'posts/:id/edit', to: 'posts#edit', as: :edit_post
-  patch 'posts/:id', to: 'posts#update'
-  delete 'posts/:id', to: 'posts#destroy', as: :delete_post
+#   get 'posts', to: 'posts#index'
+#   post 'posts', to: 'posts#create'
+#   get 'posts/new', to: 'posts#new'
+#   get 'posts/:id', to: 'posts#show', as: :post
+#   get 'posts/:id/edit', to: 'posts#edit', as: :edit_post
+#   patch 'posts/:id', to: 'posts#update'
+#   delete 'posts/:id', to: 'posts#destroy', as: :delete_post
+  resources :posts
 end
